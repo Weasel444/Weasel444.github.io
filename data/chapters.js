a1ef3432d3d305873104a2a522b15f3051d0faab
Weasel <135141502+Weasel444@users.noreply.github.com>
2026-03-23 12:40:58 +0100

    Create chapters.js

---
+++ data/chapters.js
@@ -0,0 +1,44 @@
+const CHAPTERS = [
+  {
+    id: "chapter01",
+    title: "Chapter 01",
+    folder: "chapters/chapter01",
+    count: 58
+  },
+  {
+    id: "chapter02",
+    title: "Chapter 02",
+    folder: "chapters/chapter02",
+    count: 44
+  },
+  {
+    id: "chapter03",
+    title: "Chapter 03",
+    folder: "chapters/chapter03",
+    count: 45
+  },
+  {
+    id: "chapter04",
+    title: "Chapter 04",
+    folder: "chapters/chapter04",
+    count: 51
+  },
+  {
+    id: "chapter05",
+    title: "Chapter 05",
+    folder: "chapters/chapter05",
+    count: 42
+  },
+  {
+    id: "chapter06",
+    title: "Chapter 06",
+    folder: "chapters/chapter06",
+    count: 55
+  },
+  {
+    id: "chapter07",
+    title: "Chapter 07",
+    folder: "chapters/chapter07",
+    count: 31
+  }
+];

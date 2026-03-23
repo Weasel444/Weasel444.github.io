f2eacdb8b532622f3bf6a389b5bfbed67b7d572d
Weasel <135141502+Weasel444@users.noreply.github.com>
2026-03-23 12:53:45 +0100

    Update chapters.js

--- data/chapters.js
+++ data/chapters.js
@@ -2,43 +2,50 @@ const CHAPTERS = [
   {
     id: "chapter01",
     title: "Chapter 01",
-    folder: "chapters/chapter01",
+    folder: "Chapter 01",
+    prefix: "Chapter 01_",
     count: 58
   },
   {
     id: "chapter02",
     title: "Chapter 02",
-    folder: "chapters/chapter02",
+    folder: "Chapter 02",
+    prefix: "Chapter 02_",
     count: 44
   },
   {
     id: "chapter03",
     title: "Chapter 03",
-    folder: "chapters/chapter03",
+    folder: "Chapter 03",
+    prefix: "Chapter 03_",
     count: 45
   },
   {
     id: "chapter04",
     title: "Chapter 04",
-    folder: "chapters/chapter04",
+    folder: "Chapter 04",
+    prefix: "Chapter 04_",
     count: 51
   },
   {
     id: "chapter05",
     title: "Chapter 05",
-    folder: "chapters/chapter05",
+    folder: "Chapter 05",
+    prefix: "Chapter 05_",
     count: 42
   },
   {
     id: "chapter06",
     title: "Chapter 06",
-    folder: "chapters/chapter06",
+    folder: "Chapter 06",
+    prefix: "Chapter 06_",
     count: 55
   },
   {
     id: "chapter07",
     title: "Chapter 07",
-    folder: "chapters/chapter07",
+    folder: "Chapter 07",
+    prefix: "Chapter 07_",
     count: 31
   }
 ];

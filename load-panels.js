77026ac90962585d07cda4f0789d7d5df6987550
Weasel <135141502+Weasel444@users.noreply.github.com>
2026-04-06 14:12:47 +0200

    changing back to BG IMG

--- load-panels.js
+++ load-panels.js
@@ -3,11 +3,13 @@ function loadPanels(folder, baseName, totalImages, extension) {
 
   for (let i = 1; i <= totalImages; i++) {
     const num = String(i).padStart(3, '0');
-    
-    const img = document.createElement("img");
-    img.src = `${encodeURI(folder)}/${encodeURI(baseName)}_${num}.${extension}`;
-    img.className = "panel";
-        
-    container.appendChild(img);
+
+    const panel = document.createElement("div");
+    panel.className = "panel";
+
+    const path = `${folder}/${baseName}_${num}.${extension}`;
+    panel.style.backgroundImage = `url("${encodeURI(path)}")`;
+
+    container.appendChild(panel);
   }
 }

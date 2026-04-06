4a161d4845446b61352c719a4babd55bcb29a76d
Weasel <135141502+Weasel444@users.noreply.github.com>
2026-04-06 21:55:48 +0200

    Update load-panels.js

--- load-panels.js
+++ load-panels.js
@@ -3,13 +3,19 @@ function loadPanels(folder, baseName, totalImages, extension) {
 
   for (let i = 1; i <= totalImages; i++) {
     const num = String(i).padStart(3, '0');
+    const path = `${encodeURI(folder)}/${encodeURI(baseName)}_${num}.${extension}`;
 
     const panel = document.createElement("div");
     panel.className = "panel";
 
-    const path = `${encodeURI(folder)}/${encodeURI(baseName)}_${num}.${extension}`;
-    panel.style.backgroundImage = `url("${encodeURI(path)}")`;
-
+    const img = new Image();
+    img.onload = function () {
+      const ratio = this.height / this.width;
+      panel.style.paddingTop = `${ratio * 100}%`; // keeps ratio
+      panel.style.backgroundImage = `url("${encodeURI(path)}")`;
+    };
+    img.src = path;
+    
     container.appendChild(panel);
   }
 }

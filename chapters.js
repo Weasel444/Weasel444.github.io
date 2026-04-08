35a904c6cffdb6046e7ef5478fa27c2b3ba4f0d3
Weasel <135141502+Weasel444@users.noreply.github.com>
2026-04-08 15:45:21 +0200

    Add CHAPTERS array with chapter details

---
+++ chapters.js
@@ -0,0 +1,9 @@
+const CHAPTERS = [
+  { id: "chapter01", folder: "Chapter 01", prefix: "Chapter 01_", count: 58 },
+  { id: "chapter02", folder: "Chapter 02", prefix: "Chapter 02_", count: 44 },
+  { id: "chapter03", folder: "Chapter 03", prefix: "Chapter 03_", count: 45 },
+  { id: "chapter04", folder: "Chapter 04", prefix: "Chapter 04_", count: 51 },
+  { id: "chapter05", folder: "Chapter 05", prefix: "Chapter 05_", count: 42 },
+  { id: "chapter06", folder: "Chapter 06", prefix: "Chapter 06_", count: 55 },
+  { id: "chapter07", folder: "Chapter 07", prefix: "Chapter 07_", count: 31 }
+];
